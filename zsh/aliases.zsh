e9513e03556eb2b8fdd84f73504bb3312675a9d0
Chris Baswell <me@chrisbaswell.com>
2021-02-18 08:43:25 -0600

    first commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,44 @@
+alias c='clear'
+alias artisan='php artisan'
+alias art='artisan'
+alias acc='art cache:clear'
+alias phpunit='vendor/bin/phpunit'
+alias p='phpunit'
+alias pf='p --filter'
+alias pc='p --cache-result --order-by=depends,defects --stop-on-defect'
+alias pegs='p --exclude-group slow'
+alias mfs='art migrate:fresh --seed'
+alias comp='composer'
+alias ip='curl icanhazip.com'
+alias ip4='curl v4.icanhazip.com'
+alias ip6='curl v6.icanhazip.com'
+alias ls="ls -aFG"
+alias l="ls"
+alias rm="trash"
+alias weather='curl -s wttr.in | sed -n "1,7p"'
+alias dot='cd ~/.dotfiles'
+alias o='open .'
+alias sites='cd ~/Sites'
+alias mix='npm run dev'
+alias mw='npm run watch'
+alias imix='npm install && mix'
+alias imw='npm install && mw'
+alias gs='git status'
+alias gss='gs -s'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gc="git commit -m"
+alias gco="git checkout"
+alias gcod='gco $(gdb)'
+alias gap="git add -p"
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gpsh='git push'
+alias nah='git reset --hard; git clean -df;'
+alias glt='git describe --tags --abbrev=0'
+alias gt='gittower .'
+alias gdb='git remote show origin | grep "HEAD branch" | cut -d " " -f5'
+alias gpr='gh pr checkout'
+alias wip="git add . && git commit -m 'wip'"
+alias wipa="git add . && git commit --amend -m 'wip'"
+alias cdr='cd $(git rev-parse --show-toplevel)'
+alias ray='comp require spatie/laravel-ray'
---
+++ zsh/functions.zsh
@@ -0,0 +1,51 @@
+# Switches to the default branch and deletes the branch it was just on.
+# gdbard = "Git Delete Branch And Return to Default"
+function gdbard {
+    branch=$(git branch --show-current)
+    default_branch=$(gdb)
+    git checkout $default_branch
+    git branch -D $branch
+}
+
+function tinker {
+	if [ -e artisan ]; then
+		php artisan tinker
+	elif [ -e please ]; then
+		php please tinker
+	else
+		psysh
+	fi
+}
+
+
+opendb () {
+   [ ! -f .env ] && { echo "No .env file found."; exit 1; }
+
+   DB_CONNECTION=$(grep DB_CONNECTION .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+   DB_HOST=$(grep DB_HOST .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+   DB_PORT=$(grep DB_PORT .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+   DB_DATABASE=$(grep DB_DATABASE .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+   DB_USERNAME=$(grep DB_USERNAME .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+   DB_PASSWORD=$(grep DB_PASSWORD .env | grep -v -e '^\s*#' | cut -d '=' -f 2-)
+
+   DB_URL="${DB_CONNECTION}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+
+   echo "Opening ${DB_URL}"
+   open $DB_URL
+}
+
+
+compv() {
+  if [[ $1 == *"/"* ]]; then
+    composer show $1 | grep 'versions' | grep -o -E '\*\ .+' | cut -d' ' -f2 | cut -d',' -f1;
+  else
+    composer info | grep $1
+  fi
+}
+
+
+sz() {
+  echo 'Sourcing ~/.zshrc...'
+  source ~/.zshrc
+  echo 'Done.'
+}
